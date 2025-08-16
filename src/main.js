9eed5ef3e9ab93ff33d62f97d7817a02b51b56c7
Oren <orenz0@protonmail.com>
2025-08-16 21:57:34 +0300

    fix NostrTools and source

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import './style.css';
 
 import Alpine from 'alpinejs';
 import * as NostrTools from 'nostr-tools/pure';
+import * as nip19 from 'nostr-tools/nip19';
+import * as nip04 from 'nostr-tools/nip04';
+import * as nip44 from 'nostr-tools/nip44';
 import QRCode from 'qrcode';
 
 // Make Alpine available globally for debugging
 window.Alpine = Alpine;
-window.NostrTools = NostrTools;
+window.NostrTools = {
+  ...NostrTools,
+  nip19,
+  nip04,
+  nip44,
+};
 window.QRCode = QRCode;
 
 window.appVersion = import.meta.env.VITE_APP_VERSION;
